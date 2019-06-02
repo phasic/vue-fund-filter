4b335e55edcc8237f992098cf597c4af2ce95e1c
yoran <yoran@cobben.com>
2019-06-02 16:23:11 +0200

    layout fixes

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,29 @@
 
     <v-content>
       <div class="padding-top">
-        <RangeSlider
+       
+        <v-container grid-list-md text-xs-center>
+          <v-layout row wrap>
+            <v-flex xs6>
+                <SectorFilter
+                  v-on:changedSectorFilter="sectorFilterChanged"
+                  v-bind:sectors="sectors"
+                />
+            </v-flex>
+            <v-flex xs6>
+                <CurrencyFilter
+                  v-on:changedCurrencyFilter="currencyFilterChanged"
+                  v-bind:currencies="currencies"
+                />
+            </v-flex>
+          </v-layout>
+        </v-container>
+         <RangeSlider
           v-on:changedSelection="rangeChanged"
           v-bind:min="min"
           v-bind:max="max"
           v-bind:selectedMin="selectedMin"
         />
-        <SectorFilter 
-        v-on:changedSectorFilter="sectorFilterChanged"
-        v-bind:sectors="sectors"/>
-        <CurrencyFilter 
-        v-on:changedCurrencyFilter="currencyFilterChanged"
-        v-bind:currencies="currencies"/>
         <DataTable v-bind:instruments="filteredData"/>
       </div>
     </v-content>
@@ -53,7 +64,7 @@ export default {
       sectors: this._getDefaultSectors(),
       selectedSectors: this._getDefaultSectors(),
       currencies: this._getDefaultCurrencies(),
-      selectedCurrencies: this._getDefaultCurrencies(),
+      selectedCurrencies: this._getDefaultCurrencies()
     };
   },
   computed: {
@@ -61,7 +72,12 @@ export default {
       return this._getMockData();
     },
     filteredData: function() {
-      return this._filterData(this.selectedMin, this.selectedMax, this.selectedSectors, this.selectedCurrencies);
+      return this._filterData(
+        this.selectedMin,
+        this.selectedMax,
+        this.selectedSectors,
+        this.selectedCurrencies
+      );
     }
   },
   methods: {
@@ -93,17 +109,29 @@ export default {
     rangeChanged(payload) {
       [this.selectedMin, this.selectedMax] = payload.range;
     },
-    sectorFilterChanged(payload){
+    sectorFilterChanged(payload) {
       this.selectedSectors = [...payload.sectors];
     },
-    currencyFilterChanged(payload){
-      console.log('in currencyfilterchanged');
+    currencyFilterChanged(payload) {
+      console.log("in currencyfilterchanged");
       this.selectedCurrencies = [...payload.currencies];
     },
     _filterData(min, max, sectors, currencies) {
-      const valuationFiltered = this.data.filter(el => el.valuation >= min && el.valuation <= max);
-      const sectorFiltered = sectors.length === 0 ? valuationFiltered : valuationFiltered.filter(el => sectors && sectors.indexOf(el.sector) !== -1);
-      const currencyFiltered = currencies.length === 0 ? sectorFiltered : sectorFiltered.filter(el => currencies && currencies.indexOf(el.currency) !== -1);
+      const valuationFiltered = this.data.filter(
+        el => el.valuation >= min && el.valuation <= max
+      );
+      const sectorFiltered =
+        sectors.length === 0
+          ? valuationFiltered
+          : valuationFiltered.filter(
+              el => sectors && sectors.indexOf(el.sector) !== -1
+            );
+      const currencyFiltered =
+        currencies.length === 0
+          ? sectorFiltered
+          : sectorFiltered.filter(
+              el => currencies && currencies.indexOf(el.currency) !== -1
+            );
       return currencyFiltered;
     }
   }
